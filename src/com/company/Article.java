9cdbb332dc30bed71795c7992b66753076b72eb3
sjparkk <sjparkk0820@naver.com>
2021-12-23 20:22:56 +0900

    feat : Article 클래스 멤버 변수 및 생성자 정의

--- src/com/company/Article.java
+++ src/com/company/Article.java
@@ -3,5 +3,19 @@ package com.company;
 import java.io.Serializable;
 
 public class Article implements Serializable {
+    public int seq;
+    public String writer;
+    public String message;
+
+    public Article(int seq, String writer, String message) {
+        this.seq = seq;
+        this.writer = writer;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return seq + "\t" + writer + "\t" + message;
+    }
 
 }

d93e59252c2d8dc56e21b2918a859f282b451127
sjparkk <sjparkk0820@naver.com>
2021-12-23 20:47:06 +0900

    feat : 로직 구현

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,6 +20,9 @@ public class Board {
 
         //입력을 받음
         Scanner s = new Scanner(System.in);
+        while(true) {
+            prompt(s);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -34,5 +34,44 @@ public class Board {
             ObjectInputStream ois = new ObjectInputStream(fis);) {
             list = (List<Article>) ois.readObject();
         }
+    }
+
+    private static void prompt(Scanner s) throws FileNotFoundException, IOException {
+
+        System.out.println("순서\t글쓴이\t메시지");
+        System.out.println("-----------------------");
+
+        if(list.size() == 0) {
+            System.out.println("[아직 글이 없습니다.]");
+        }
+        list.forEach(System.out::println);
+
+        System.out.println(">> 입력을 원하면 w, 종료를 원하면 x를 입력해주세요.");
+        String command = s.nextLine();
+        if(command.equalsIgnoreCase("w")) {
+            System.out.print("작성자 이름을 입력해주세요.");
+            String writer = s.nextLine();
+
+            System.out.print("글 내용을 입력해주세요.");
+            String message = s.nextLine();
+
+            int seq = 0;
+            if(list.size() != 0) {
+                seq = list.get(list.size() - 1).seq + 1;
+            }
+
+            list.add(new Article(seq, writer, message));
+            System.out.println();
+
+            // 직렬화 출력하기: 글이 작성되었으면 메모리의 객체를 직렬화한 뒤
+            // 물리적인 파일로 하드디스크에 저장한다.
+            try(FileOutputStream fos = new FileOutputStream("board.txt");
+                ObjectOutputStream oos = new ObjectOutputStream(fos);) {
+                oos.writeObject(list);
+            }
+        } else if (command.equalsIgnoreCase("x")) {
+            System.exit(0);
+        }
+
     }
 }

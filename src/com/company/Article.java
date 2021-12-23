7299272f4e2993c1825621b354b842070eb4c080
sjparkk <sjparkk0820@naver.com>
2021-12-23 20:54:18 +0900

    feat : transient 주석 추가

--- src/com/company/Article.java
+++ src/com/company/Article.java
@@ -7,6 +7,7 @@ public class Article implements Serializable {
     private static final long serialVersionUID = 810939092043026342L;
     public int seq;
     public String writer;
+    // 직렬화 시 transient가 붙은 대상을 제외하고 직렬화한다. transient필드는 역직렬화 시 null 또는 타입의 기본값으로 반환된다.
     public transient String message;
 
     public Article(int seq, String writer, String message) {
